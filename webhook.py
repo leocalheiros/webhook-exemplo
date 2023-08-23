a958044388424fa8feae25cf2e630b01c29971eb
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-08-23 10:19:07 -0300

    upload project

---
+++ webhook.py
@@ -0,0 +1,13 @@
+#enviar webhook
+
+import requests
+import json
+
+webhook_url = 'http://127.0.0.1:5000/webhook'
+
+data = {
+    "name" : "brad",
+    "Channel URL" : "test url"
+}
+
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
